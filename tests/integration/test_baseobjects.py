ea579d22f912c847653db608c458a7c316b15b4b
Matthias Sturm <m.sturm@click-it.online>
2021-12-09 18:39:31 +0100

    Add CSS class check to Base Integration Tests

--- tests/integration/test_baseobjects.py
+++ tests/integration/test_baseobjects.py
@@ -1,9 +1,12 @@
+# TODO: Pull all hardcoded CSS identifiers from JSON configs
+
 import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import json
 
 @pytest.fixture
 
@@ -12,21 +15,32 @@ def config():
 	config["wait"] = 10
 	config["driver"] = webdriver.Chrome()
 	config["driver"].get("https://x0-test.webcodex.de/");
+
+	config["json"] = {}
+	with open("tests/integration/config/basic/object.json") as file:
+		config["json"]["object"] = json.load(file)
+		file.close()
+
 	return config
 
 
 class TestBaseObjectsExistence:
 	def test_button(self, config):
-		"""Check if button element exists on site"""
+		"""Check if button element exists on site, have proper class attribute"""
 		d = config["driver"]
 		wait = WebDriverWait(d, config["wait"])
 		elem = wait.until(EC.presence_of_element_located(
 			(By.CSS_SELECTOR, "#Test1_Button1 #Test1_Button1_SQLText")
 		))
+
+		el = d.find_element(By.CSS_SELECTOR, "#Test1_Button1")
+		val = el.get_attribute("class")
+		assert val == config["json"]["object"]["Button1"]["Attributes"]["Style"], "Button has improper class attribute, differs from JSON config."
+
 		d.close()
 
 	def test_formfield(self, config):
-		"""Check if formfield element (includes field and pulldown) exists on site"""
+		"""Check if formfield element (includes field and pulldown) exists on site, have proper class attribute"""
 		d = config["driver"]
 		wait = WebDriverWait(d, config["wait"])
 		# field (aka input)
@@ -37,24 +51,43 @@ class TestBaseObjectsExistence:
 		elem = wait.until(EC.presence_of_element_located(
 			(By.CSS_SELECTOR, "#Test1_FormFieldList1 #Test1_FormFieldList1_FormFieldPulldown1 #FormFieldPulldown1")
 		))
+
+		el = d.find_element(By.CSS_SELECTOR, "#FormField1")
+		val = el.get_attribute("class")
+		assert val == config["json"]["object"]["FormField1"]["Attributes"]["Style"], "FormField has improper class attribute, differs from JSON config."
+
+		el = d.find_element(By.CSS_SELECTOR, "#FormFieldPulldown1")
+		val = el.get_attribute("class")
+		assert val == config["json"]["object"]["FormFieldPulldown1"]["Attributes"]["Style"], "FormFieldPulldown has improper class attribute, differs from JSON config."
+
 		d.close()
 
 	def test_sqltext(self, config):
-		"""Check if SQLText element exists on site"""
+		"""Check if SQLText element exists on site, have proper class attribute"""
 		d = config["driver"]
 		wait = WebDriverWait(d, config["wait"])
 		elem = wait.until(EC.presence_of_element_located(
 			(By.CSS_SELECTOR, "#Test1_SQLText1")
 		))
+
+		el = d.find_element(By.CSS_SELECTOR, "#Test1_SQLText1")
+		val = el.get_attribute("class")
+		assert val == config["json"]["object"]["SQLText1"]["Attributes"]["Style"], "SQLText has improper class attribute, differs from JSON config."
+
 		d.close()
 
 	def test_list(self, config):
-		"""Check if list element exists on site"""
+		"""Check if list element exists on site, have proper class attribute"""
 		d = config["driver"]
 		wait = WebDriverWait(d, config["wait"])
 		elem = wait.until(EC.presence_of_element_located(
 			(By.CSS_SELECTOR, "#Test1_ServiceConnector1 #Test1_ServiceConnector1_List1 #Test1_ServiceConnector1_List1_List1HdrRow")
 		))
+
+		el = d.find_element(By.CSS_SELECTOR, "#Test1_ServiceConnector1_List1")
+		val = el.get_attribute("class")
+		assert val == config["json"]["object"]["List1"]["Attributes"]["Style"], "List has improper class attribute, differs from JSON config."
+
 		d.close()
 
 
