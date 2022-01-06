220305152efb331e692ddb2acbc2d02c7409ac3c
Claus Prüfer <c.pruefer@click-it.online>
2022-01-06 20:28:23 +0100

    Add 3 examples

---
+++ example/list_detail_switch_screen/sql/insert-txt.sql
@@ -0,0 +1,7 @@
+INSERT INTO webui.text (id, "group", value_de) VALUES ('TXT.MENU.LISTDETAILVIEW.SCREEN1', 'menu', 'Link 1 to Screen 1');
+INSERT INTO webui.text (id, "group", value_de) VALUES ('TXT.MENU.LISTDETAILVIEW.SCREEN2', 'menu', 'Link 2 to Screen 2');
+
+INSERT INTO webui.text (id, "group", value_de) VALUES ('TXT.TABLE.LISTDETAILVIEW.HEADER.COL1', 'tableheader', 'Table Column 1');
+INSERT INTO webui.text (id, "group", value_de) VALUES ('TXT.TABLE.LISTDETAILVIEW.HEADER.COL2', 'tableheader', 'Table Column 2');
+
+INSERT INTO webui.text (id, "group", value_de) VALUES ('TXT.CONTEXTMENU.LISTDETAILVIEW.EDIT', 'contextmenu', 'Edit Dataset');
