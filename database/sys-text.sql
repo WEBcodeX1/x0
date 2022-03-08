9251b9aa8d2af2c450e2743c02f756c247ebd982
Claus Prüfer <c.pruefer@click-it.online>
2022-03-08 13:56:17 +0100

    Add missing base text (german)

--- database/sys-text.sql
+++ database/sys-text.sql
@@ -1,1 +1,4 @@
-INSERT INTO webui.text (id, "group", value_de) VALUES ('TXT.SYS.OVERLAY-CLOSE', 'OVERLAY', 'Fenster Schließen');
+INSERT INTO webui.text (id, "group", value_de) VALUES ('SYS_DEFAULT_ERROR_TEXT', 'Error', 'Die Formularfeldüberprüfung hat einen Fehler ergeben.');
+INSERT INTO webui.text (id, "group", value_de) VALUES ('TXT.SYS.OVERLAY-CLOSE', 'Overlay', 'Fenster Schließen');
+INSERT INTO webui.text (id, "group", value_de) VALUES ('TXT.SYS.ERROR.FORMVALIDATE.FIELD-OR', 'Error', 'Die folgenden Felder sind entweder / oder:');
+INSERT INTO webui.text (id, "group", value_de) VALUES ('TXT.SYS.ERROR.TABLE-NO-ROWS', 'Error', 'Keine Einträge in der Tabelle vorhanden.');
