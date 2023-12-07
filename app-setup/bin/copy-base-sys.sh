d5f3918c3c3c04294be69f954793033eff48de65
Claus Prüfer (CTO) <c.pruefer@click-it.online>
2023-12-07 14:26:23 +0100

    Copy source python with correct permissions set

--- app-setup/bin/copy-base-sys.sh
+++ app-setup/bin/copy-base-sys.sh
@@ -11,14 +11,3 @@ cp -Ra ${x0DIR}/*.js ${WEBSERVERDIR}/${VHOST_DIR}/
 
 # copy x0 base python scripts
 cp -Ra ${x0DIR}/python/*.py ${WEBSERVERDIR}/${VHOST_DIR}/python/
-
-# set filesystem permissions
-chown -R www-data:www-data /var/www/python
-chmod 770 /var/www/python
-find /var/www/python -type f -print0 | xargs -0 chmod 460
-chown -R www-data:www-data /var/www/vhosts
-find /var/www/vhosts -type f -print0 | xargs -0 chmod 460
-find /var/www/vhosts -type d -print0 | xargs -0 chmod 770
-
-# reload apache
-service apache2 reload
