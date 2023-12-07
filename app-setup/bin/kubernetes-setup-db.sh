d4bf93e55abb47f19ee371b8c7c92be2b8ca5de8
Claus Prüfer (CTO) <c.pruefer@click-it.online>
2023-12-07 14:26:58 +0100

    Dynamically get database name/id from app-config.json

--- app-setup/bin/kubernetes-setup-db.sh
+++ app-setup/bin/kubernetes-setup-db.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
 APP_SETUP_DIR="/var/lib/x0/app-setup/bin/"
-CMD_PARAMETERS="x0 mypostgres"
 
 cd ${APP_SETUP_DIR}
+
+CMD_PARAMETERS="$(python3 ./get-pg-database.py) mypostgres"
 
 ./create-sys-db.sh ${CMD_PARAMETERS}
 ./init-app-db.sh ${CMD_PARAMETERS}
