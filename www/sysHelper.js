0e99265b13976e037f7df503b5bd990d279ae13a
Claus Prüfer <pruefer@webcodex.de>
2025-04-15 08:03:07 +0200

    Add array processing for templating

--- www/sysHelper.js
+++ www/sysHelper.js
@@ -16,16 +16,22 @@
 //------------------------------------------------------------------------------
 
 sysMergeObjects = function(Object1, Object2) {
-    let o1 = Object.assign({}, Object1);
-    let o2 = Object.assign({}, Object2);
-	for (var Key in o2) {
+	let o1 = Object.assign({}, Object1);
+	let o2 = Object.assign({}, Object2);
+	for (const Key in o2) {
 		// if processing type Object, recurse
 		if (o2[Key].constructor == Object) {
 			o1[Key] = sysMergeObjects(o1[Key], o2[Key]);
 		}
 		else {
-			// overwrite existent property
-			o1[Key] = o2[Key];
+			if (Array.isArray(o1[Key]) && Array.isArray(o2[Key])) {
+				// append all array elements from o2 to o1
+				o2[Key].forEach(item => o1[Key].push(item));
+			}
+			else {
+				// overwrite existent property
+				o1[Key] = o2[Key];
+			}
 		}
 	}
 	return o1;
