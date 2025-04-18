c04a8c2da90c5c16cd354ec7f2d07218f0408b31
Claus Prüfer <pruefer@webcodex.de>
2025-04-18 09:16:03 +0200

    Add copy / paste example

---
+++ example/copy_paste/sql/insert-txt.sql
@@ -0,0 +1,24 @@
+INSERT INTO webui.text (id, "group", value_en) VALUES ('TXT.TABLE.EXAMPLE-COPYPASTE.LINK1', 'menu', 'Link1');
+
+INSERT INTO webui.text (id, "group", value_en) VALUES ('TXT.TABLE.EXAMPLE-COPYPASTE.TAB1', 'tabcontainer', 'Source Data');
+INSERT INTO webui.text (id, "group", value_en) VALUES ('TXT.TABLE.EXAMPLE-COPYPASTE.TAB2', 'tabcontainer', 'Destination Data');
+
+INSERT INTO webui.text (id, "group", value_en) VALUES ('TXT.TABLE.EXAMPLE-COPYPASTE.HEADER.COL1', 'table-header', 'Col1');
+INSERT INTO webui.text (id, "group", value_en) VALUES ('TXT.TABLE.EXAMPLE-COPYPASTE.HEADER.COL2', 'table-header', 'Col2');
+INSERT INTO webui.text (id, "group", value_en) VALUES ('TXT.TABLE.EXAMPLE-COPYPASTE.HEADER.COL3', 'table-header', 'Col3');
+INSERT INTO webui.text (id, "group", value_en) VALUES ('TXT.TABLE.EXAMPLE-COPYPASTE.HEADER.COL4', 'table-header', 'Col4');
+
+INSERT INTO webui.text (id, "group", value_en) VALUES ('TXT.TABLE.EXAMPLE-COPYPASTE.PULLDOWN.OPT1', 'pulldown-option', 'Option 10');
+INSERT INTO webui.text (id, "group", value_en) VALUES ('TXT.TABLE.EXAMPLE-COPYPASTE.PULLDOWN.OPT2', 'pulldown-option', 'Option 20');
+INSERT INTO webui.text (id, "group", value_en) VALUES ('TXT.TABLE.EXAMPLE-COPYPASTE.PULLDOWN.OPT3', 'pulldown-option', 'Option 30');
+
+INSERT INTO webui.text (id, "group", value_en) VALUES ('TXT.CONTEXTMENU.EXAMPLE-COPYPASTE.GETDATA', 'context-menu', 'Get Data');
+INSERT INTO webui.text (id, "group", value_en) VALUES ('TXT.CONTEXTMENU.EXAMPLE-COPYPASTE.SETDATA', 'context-menu', 'Set Data');
+
+INSERT INTO webui.text (id, "group", value_en) VALUES ('TXT.TABLE.EXAMPLE-COPYPASTE.FORM1-LABEL', 'context-menu', 'Column 1');
+INSERT INTO webui.text (id, "group", value_en) VALUES ('TXT.TABLE.EXAMPLE-COPYPASTE.FORM2-LABEL', 'context-menu', 'Column 2');
+INSERT INTO webui.text (id, "group", value_en) VALUES ('TXT.TABLE.EXAMPLE-COPYPASTE.FORM3-LABEL', 'context-menu', 'Column 3');
+INSERT INTO webui.text (id, "group", value_en) VALUES ('TXT.TABLE.EXAMPLE-COPYPASTE.FORM4-LABEL', 'context-menu', 'Column 4');
+
+INSERT INTO webui.text (id, "group", value_en) VALUES ('TXT.TABLE.EXAMPLE-COPYPASTE.FORMLIST1.HEADER', 'form-section', 'Destination');
+INSERT INTO webui.text (id, "group", value_en) VALUES ('TXT.TABLE.EXAMPLE-COPYPASTE.FORMLIST1.SUBHEADER', 'form-section', 'Destination Formlist');
