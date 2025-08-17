f42676393114944c3ed437165e658e1a3c758406
Claus <pruefer@webcodex.de>
2025-08-17 19:42:35 +0200

    Add example for sysObjOpenClose

---
+++ example/open_close_container/README.md
@@ -0,0 +1,12 @@
+# Example #14 (Open Close Container)
+
+This example demonstrates the use of the OpenCloseContainer
+**x0-system-object** in the x0 JavaScript SPA Browser Framework.
+
+## URL
+
+Open URL: `http://x0-app.x0.localnet/python/Index.py?appid=example14`
+
+---
+
+**Note:** *x0-app* and *x0-db* docker containers must be up and running for viewing.
---
+++ example/open_close_container/sql/insert-txt.sql
@@ -0,0 +1,11 @@
+INSERT INTO webui.text (id, "group", value_en) VALUES ('TXT.MENU.LINK1', 'link', 'Link 1');
+INSERT INTO webui.text (id, "group", value_en) VALUES ('TXT.OPENCLOSE1-HEADER', 'sqltext', 'Test Header 1');
+INSERT INTO webui.text (id, "group", value_en) VALUES ('TXT.TEXT1', 'sqltext', 'Test Content 1');
+INSERT INTO webui.text (id, "group", value_en) VALUES ('TXT.TEXT2', 'sqltext', 'Test Content 2');
+INSERT INTO webui.text (id, "group", value_en) VALUES ('TXT.OPENCLOSE2-HEADER', 'sqltext', 'Test Header 2');
+INSERT INTO webui.text (id, "group", value_en) VALUES ('TXT.TEXT3', 'sqltext', 'Test Content 3');
+INSERT INTO webui.text (id, "group", value_en) VALUES ('TXT.TEXT4', 'sqltext', 'Test Content 4');
+INSERT INTO webui.text (id, "group", value_en) VALUES ('TXT.OPENCLOSE3-HEADER', 'sqltext', 'Test Header 3');
+INSERT INTO webui.text (id, "group", value_en) VALUES ('TXT.TEXT5', 'sqltext', 'Test Content 5');
+INSERT INTO webui.text (id, "group", value_en) VALUES ('TXT.TEXT6', 'sqltext', 'Test Content 6');
+
