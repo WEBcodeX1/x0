7a4f44c16b598b80a92b5d1be1b2727e317d9938
Claus <pruefer@webcodex.de>
2025-08-28 13:17:28 +0200

    Add system object tree (simple) example

---
+++ example/tree_simple/sql/insert-txt.sql
@@ -0,0 +1,9 @@
+INSERT INTO webui.text (id, "group", value_en) VALUES ('TXT.MENU.LINK1', 'link', 'Link 1');
+INSERT INTO webui.text (id, "group", value_en) VALUES ('TXT.NODE1', 'sqltext', 'Node-1');
+INSERT INTO webui.text (id, "group", value_en) VALUES ('TXT.NODE2', 'sqltext', 'Node-2');
+INSERT INTO webui.text (id, "group", value_en) VALUES ('TXT.ITEM1', 'sqltext', 'Item-1');
+INSERT INTO webui.text (id, "group", value_en) VALUES ('TXT.ITEM2', 'sqltext', 'Item-2');
+INSERT INTO webui.text (id, "group", value_en) VALUES ('TXT.ITEM3', 'sqltext', 'Item-3');
+INSERT INTO webui.text (id, "group", value_en) VALUES ('TXT.ITEM4', 'sqltext', 'Item-4');
+INSERT INTO webui.text (id, "group", value_en) VALUES ('TXT.ITEM5', 'sqltext', 'Item-5');
+INSERT INTO webui.text (id, "group", value_en) VALUES ('TXT.ITEM6', 'sqltext', 'Item-6');
